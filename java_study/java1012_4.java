2f2dae6fae6ec995f2f67985a862f5ba30244cc7
BoYoung00 <qhdud20@naver.com>
2022-10-12 15:13:07 +0900

    자바 강의

--- java_study/java1012_4.java
+++ java_study/java1012_4.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class java1012_4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("숫자를 여러 개 입력 ");
+        System.out.print("숫자를 여러 개 입력 : ");
         String a = in.nextLine();
 
-        char[] aa = a.toCharArray();
-
-        for(int i = 0; i < aa.length; i++) {
-            System.out.println(aa[i]);
+        for(int i = 0; i < a.length(); i++) {
+            int num = (int)a.charAt(i) - 48;
+            System.out.println("*".repeat(num));
         }
+
+        //        char[] aa = a.toCharArray();
+//
+//        for(int i = 0; i < aa.length; i++) {
+//            int num = (int)aa[i] - 48;
+//            System.out.println("*".repeat(num));
+//        }
     }
 }
