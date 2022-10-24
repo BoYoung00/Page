ce38c873a97207a07b041ce2b54c58b5f0166cf1
BoYoung00 <qhdud20@naver.com>
2022-10-24 09:09:28 +0900

    자바
    자바 수업

--- java_study/java1012_3.java
+++ java_study/java1012_3.java
@@ -6,6 +6,24 @@ public class java1012_3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.print("정수? ");
+        int a = in.nextInt();
+
+        while (a!=0) {
+            int sum = 0;
+
+            for (int i=1; i <= a; i++) {
+                sum +=i;
+                if (i==a)
+                    System.out.printf("1~%d까지의 합 : %d\n", a, sum);
+            }
+            System.out.print("정수? ");
+            a = in.nextInt();
+
+            if (a == 0) {
+                break;
+            }
+        }System.out.println("프로그램 종료");
 //        while (true) {
 //            System.out.print("정수? ");
 //            int a = in.nextInt();
@@ -22,23 +40,6 @@ public class java1012_3 {
 //            System.out.printf("1~%d까지의 합 : %d\n", a, sum);
 //        }
 
-        System.out.print("정수? ");
-        int a = in.nextInt();
 
-        while (a!=0) {
-            int sum = 0;
-
-            for (int i=1; i <= a; i++) {
-                sum +=i;
-                if (i==a)
-                    System.out.printf("1~%d까지의 합 : %d\n", a, sum);
-            }
-            System.out.print("정수? ");
-            a = in.nextInt();
-
-            if (a == 0) {
-            break;
-        }
-    }System.out.println("프로그램 종료");
 }
 }
